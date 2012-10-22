7677e2ff4be6d53879b0374f4de163e1e75d8174
Lee <Lee.Yerkes@gmail.com>
2012-10-21 22:59:31 -0500

    Updated queue files, still not compiling

--- q.c
+++ q.c
@@ -1,42 +1,40 @@
 #include "q.h"
 #include <semaphore.h>
-#define MAX_SIZE 10
+#include <pthread.h>
+#include <stdio.h>
 
-typedef struct synchedq {
-  char* q_data[MAX_SIZE];
-  int head;
-  int size;
-  sem_t open;
-  sem_t filled;
-} sq;
+#define int MAX_SIZE 10
 
-void sq_init(sq q){
-  head = 0;
-  size = 0;
-  sem_init(&open, 0, MAX_SIZE);
-  sem_init(&filled, 0, 0);
+void sq_init(sq* q){
+  char* new_array[MAX_SIZE]; 
+  q->data = new_array;
+  q->head = 0;
+  q->size = 0;
+  sem_init(&q->open, 0, MAX_SIZE);
+  sem_init(&q->filled, 0, 0);
 }
 
-void sq_enq(sq q, char* new_item){
-  sem_wait&(&open);
-  q_data[(head + size % MAX_SIZE)] = new_item;
-  size++;
-  sem_post(&filled); 
-  /*p(enq_wait)*/
-  /*set element at index tail to new_item pointer*/
-  /*increment tail */
+void sq_enq(sq* q, char* new_item){
+  sem_wait&(&q->open);
+
+  q->data[(q->head + q->size % MAX_SIZE)] = new_item;
+  q->size++;
+
+  sem_post(&q->filled); 
 }
 
-void* sq_deq(sq q){
-  sem_wait(&filled);
-  size--;
-  int loc = head;
-  head = (head + 1) % MAX_SIZE;
-  sem_post(&open);
-return q_data[loc]; /*return popped pointer*/
+void* sq_deq(sq* q){
+  sem_wait(&q->filled);
+
+  q->size--;
+  int loc = q->head;
+  q->head = (q->head + 1) % MAX_SIZE;
+
+  sem_post(&q->open);
+
+  return q->data[loc]; /*return popped pointer*/
 }
 
-int sq_isEmpty(sq q){
-  return 0 == size;
+int sq_isEmpty(sq* q){
+  return 0 == q->size;
 }
-/*DONT FORGET TO DESTROY SEMAPHORES*/
--- q.h
+++ q.h
@@ -1,5 +1,15 @@
-typedef struct synchedq sq
-void sq_init(sq q);
-void sq_enq(sq q, void *ptr);
-void* sq_deq(sq q);
-int sq_isEmpty(sq q);
+typedef struct {
+  char** data;
+  int head;
+  int size;
+  sem_t open;
+  sem_t filled;
+} sq;
+
+extern void sq_init(sq* q);
+
+extern void sq_enq(sq* q, void *new_item);
+
+extern void* sq_deq(sq* q);
+
+extern int sq_isEmpty(sq* q);
